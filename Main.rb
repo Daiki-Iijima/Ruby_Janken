ce8a2bab17d34fb8068e5895d5e52bce31f8bac3
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-08-10 17:36:50 +0900

    処理をメソッドに抽出

--- Main.rb
+++ Main.rb
@@ -1,3 +1,57 @@
+def player
+  get = gets
+  getNum = get.to_i
+
+  if getNum  == 1
+    puts 'ぐー'
+  elsif getNum  == 2
+    puts 'ちょき'
+  elsif getNum  == 3
+    puts 'ぱー'
+  end
+
+  return getNum
+end
+
+def enemy
+  r = rand(3) + 1
+
+  if r == 1
+    puts 'ぐー'
+  elsif r == 2
+    puts 'ちょき'
+  elsif r == 3
+    puts 'ぱー'
+  end
+
+  return r
+end
+
+def judge(enemy,player)
+  if enemy == player
+    puts '== あいこ =='
+    return true
+  elsif enemy == 1 && player == 2
+    puts  '-- コンピューターの勝ち --'
+    return false
+  elsif enemy == 1 && player == 3
+    puts  '++ 私の勝ち ++'
+    return false
+  elsif enemy == 2 && player == 1
+    puts  '++ 私の勝ち ++'
+    return false
+  elsif enemy == 2 && player == 3
+    puts  '-- コンピューターの勝ち --'
+    return false
+  elsif enemy == 3 && player == 1
+    puts  '-- コンピューターの勝ち --'
+    return false
+  elsif enemy == 3 && player == 2
+    puts  '++ 私の勝ち ++'
+    return false
+  end
+end
+
 class Main
   # じゃんけん
 
@@ -5,47 +59,13 @@ class Main
 
     puts '== じゃんけん =='
 
-    get = gets
-    getNum = get.to_i
+    playerNum = player
+    enemyNum = enemy
 
-    if getNum  == 1
-      puts 'ぐー'
-    elsif getNum  == 2
-      puts 'ちょき'
-    elsif getNum  == 3
-      puts 'ぱー'
+    if !judge(enemyNum,playerNum)
+      break;
     end
 
-    r = rand(3) + 1
-
-    if r == 1
-      puts 'ぐー'
-    elsif r == 2
-      puts 'ちょき'
-    elsif r == 3
-      puts 'ぱー'
-    end
-
-    if r == getNum
-      puts '== あいこ =='
-    elsif r == 1 && getNum == 2
-      puts  '-- コンピューターの勝ち --'
-      break
-    elsif r == 1 && getNum == 3
-      puts  '++ 私の勝ち ++'
-      break
-    elsif r == 2 && getNum == 1
-      puts  '++ 私の勝ち ++'
-      break
-    elsif r == 2 && getNum == 3
-      puts  '-- コンピューターの勝ち --'
-      break
-    elsif r == 3 && getNum == 1
-      puts  '-- コンピューターの勝ち --'
-      break
-    elsif r == 3 && getNum == 2
-      puts  '++ 私の勝ち ++'
-      break
-    end
   end
+
 end
